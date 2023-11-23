604544a9dab574f9bcfab1711f15c1dd44cfb9cb
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-11-23 20:54:48 +0200

    implements db interface

---
+++ src/classes/database.ts
@@ -0,0 +1,76 @@
+import { fixName } from "../functions/utils";
+import { ICollection } from "../interfaces/cluster.interface";
+import { IDatabase } from "../interfaces/database.interface";
+import { ClusterConfig } from "../interfaces/database_config";
+import * as fs from 'fs';
+import { Collection } from "./collection";
+
+export interface databaseParams  {
+    name : string;
+    parentPath : string;
+    shouldGitIgnore? : true
+}
+
+export class Database implements IDatabase {
+    name: string;
+    path: string;
+
+    constructor(params : databaseParams) {
+        this.name = params.name
+        const fixedName = fixName(this.name)
+        this.path =   `${params.parentPath}/${fixedName}`;
+
+        // create dir if it doesn't exist
+        if (!this.checkIfDirExists()) {
+            this.createDir();
+        }else {
+            console.log('Directory Already Exisits');
+        }
+    }
+
+
+    /** Checks if the database dir already exists 
+     * 
+     * @return True if the directory exists
+    */
+    private checkIfDirExists() : boolean {
+        return fs.existsSync(this.path);
+    }
+
+    /** Creates a directory for the database to live in */
+    private createDir() : void {
+        const dir = fs.mkdir(this.path, (err) => {
+            if (err != null) {
+                console.log(err);
+            } else {
+                console.log('Directory Made!');
+                
+            }
+        })
+    }
+
+    createCollection(config: ClusterConfig): ICollection {
+        const colRef = new Collection({
+            name : config.name,
+            path : `${this.path}`
+        })
+
+        return colRef;
+    }
+
+    deleteCollection(colName: string): boolean {
+        throw new Error("Method not implemented.");
+    }
+
+    getCollection(colName: string): ICollection {
+        const colRef = new Collection({
+            name : colName,
+            path : `${this.path}`
+        })
+
+        return colRef;
+    }
+
+
+
+}
