a422097baaa7d4ff924a97304588e8d0664854f6
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-11-26 01:28:13 +0200

    fix: types for database config interface

--- src/interfaces/database_config.ts
+++ src/interfaces/database_config.ts
@@ -1,12 +1,12 @@
 export interface DatabaseConfig {
     name : string,
-    shouldGitIgnore : true,
+    shouldGitIgnore : boolean,
 
     /** Time added to each operation to simulate the delay of network calls
      * 
      * `0` by default which means there is no delay added
      */
-    simulatedDelayTime : 0
+    simulatedDelayTime : number 
 }
 
 export interface ClusterConfig {
