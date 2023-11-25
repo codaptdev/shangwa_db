48ba9c91b2b1cb53716883a62fa98780bc1970ae
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-11-26 01:27:11 +0200

    fix: removes multiple file system from factory methods

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { Database } from "./classes/database";
+import { SingleFileDatabase } from "./classes/single_file_sys/single_file_database";
 import { setUpShangwaDB, getDatabasesRootPath } from "./functions/create_db";
 import { DatabaseConfig } from "./interfaces/database_config";
 import * as fs from 'fs';
 
 export interface DocumentData {
-    id : string,
     jsonStr : string
 }
     /** Sets up a database in your working directory. If you specify to gitIgnore the databases
@@ -15,10 +14,10 @@ export interface DocumentData {
  * @param name - uniquely identifies the database
  * @param shouldGitIgnore - a flag for Shangwa DB to git ignore your database or not 
  */
-export function createDatabase(config : DatabaseConfig) : Database {
+export function createDatabase(config : DatabaseConfig) : SingleFileDatabase {
     setUpShangwaDB()
 
-    const db = new Database({
+    const db = new SingleFileDatabase({
         name : config.name,
         parentPath : getDatabasesRootPath(),
         shouldGitIgnore : config.shouldGitIgnore
@@ -30,28 +29,33 @@ export function createDatabase(config : DatabaseConfig) : Database {
 /** Attempts to get an already existting datbase with the name passed
  * to the function. If the database is not found null is returned;
  */
-export function getExistingDatabase(name : string) : Database | undefined {
+export function getExistingDatabase(name : string) : SingleFileDatabase {
 
-    const dbPath = `${getDatabasesRootPath}/${name}`
-    const gitIgnoreedPath = `${getDatabasesRootPath}/.${name}`
+    const dbPath = `${getDatabasesRootPath()}/${name}`
     const exist = fs.existsSync(dbPath)
-    const ignoredExists = fs.existsSync(gitIgnoreedPath)
 
-    if (exist || ignoredExists) {
-        if(exist) {
-            return new Database({
-                name : name,
-                parentPath : getDatabasesRootPath()
-            })
-        } else {
-            return new Database({
-                name : name,
-                parentPath : getDatabasesRootPath()
-            })
-        }
-    } else {
-        return undefined
+    // If database doesn't exists throw error
+    if (!exist) {
+        throw new DatabaseNotFound(name)
     }
+
+    return new SingleFileDatabase({
+        name : name,
+        parentPath : getDatabasesRootPath(),
+        shouldGitIgnore : true
+    })
+}
+
+class DatabaseNotFound implements Error {
+
+    name: string = 'Database not found'
+    message: string;
+    stack?: string | undefined;
+
+    constructor(name : string) {
+        this.message = `The database "${name}" doesn't exist"`
+    }
+
 }
 
 
